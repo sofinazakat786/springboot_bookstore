e580f3711f186ae66011e6386bb65443987adeac
Nazakat Nazir Sofi <sofinazakat786@gmail.com>
2025-02-14 00:22:38 +0530

    book updated

--- src/main/java/com/api/book/bootrestbook/model/Book.java
+++ src/main/java/com/api/book/bootrestbook/model/Book.java
@@ -3,7 +3,6 @@ package com.api.book.bootrestbook.model;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,62 +13,62 @@ import jakarta.persistence.Table;
 @Table(name = "books")
 public class Book {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "book_id")
-	private int id;
-	
-	@Column(name = "title")
-	private String title;
-	
-	@OneToOne(cascade = CascadeType.ALL)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "book_id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @OneToOne(cascade = CascadeType.ALL)
 //	@OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
-	private Author author;
+    private Author author;
 
-	// Constructor
-	public Book(int id, String title, Author author) {
-		this.id = id;
-		this.title = title;
-		this.author = author;
-	}
+    // Constructor
+    public Book(int id, String title, Author author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
 
-	// Default Constructor
-	public Book(String title, Author author) {
-		this.title = title;
-		this.author = author;
-	}
+    // Default Constructor
+    public Book(String title, Author author) {
+        this.title = title;
+        this.author = author;
+    }
 
-	// getters and setters
-	public Book() {
-	}
+    // getters and setters
+    public Book() {
+    }
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public Author getAuthor() {
-		return author;
-	}
+    public Author getAuthor() {
+        return author;
+    }
 
-	public void setAuthor(Author author) {
-		this.author = author;
-	}
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
 
-	// toString method
-	@Override
-	public String toString() {
-		return "Book [id=" + id + ", title=" + title + ", author=" + author + "]";
-	}
+    // toString method
+    @Override
+    public String toString() {
+        return "Book [id=" + id + ", title=" + title + ", author=" + author + "]";
+    }
 }
